dd19ba5dd60bff7e0a25dc1d16c0062da819b1b1
shikshita23 <shikshitasubedi1@gmail.com>
2023-11-17 14:28:10 +0545

    css updated

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -2,43 +2,42 @@ import { useState } from 'react'
 import './Quiz.css'
 import questions from './questions'
 import Second from './result';
-
 function Quiz(){
     const[question,setQuestion]=useState(0)
     const [showResult,setShowResult]=useState(false);
     const [clickedOption,setClickedOption]=useState(0);
-
     const nextQuestion=() =>{
         checkScore();
         if(question<questions.length-1){
-            
+            clearRadio();
             setQuestion(question+1)
+            setClickedOption(0);
         }
         else{
             setShowResult(true)
-        }   
+        }
+    }
+    function clearRadio(){
+        let radio = document.getElementById("radio_btn");
+        radio.checked = false;
     }
     const[score,setScore]=useState(0)
-
     const checkScore=()=>{
         if(clickedOption===questions[question].answer){
             setScore(score+1);
         }
     }
     const func =(i) =>{
-        console.log(i)
         setClickedOption(i+1)
-        
     }
     return(
         <div>
-            <div className="Heading">Quiz On Nepals History</div>
+            <div className="Heading">Quiz</div>
             <div className="quizBoard">
                 {showResult?(
                     <Second score={score} totalScore={questions.length}/>
                     ):(
-                 <>
-                    
+                 <> 
                 <div className="questionSection">
                    <div>{questions[question].qn} </div>      
                 </div>
@@ -46,12 +45,12 @@ function Quiz(){
                     {questions[question].options.map((option,i )=> {
                         return(
                             <>
-                             <label> <input className="options" type='radio' value={option} name='options' key={i} onChange={()=>func(i)}/>{option}</label>  
+                             <label> <input id="radio_btn" className="options" type='radio' value={option} name='options' key={i} onChange={()=>func(i)}/>{option}</label>  
                             </>
                         )
                     })}
                 </div>
-                <div>
+                <div className='nextSection'>
                     <button className='nextButton' onClick={nextQuestion}>Next</button>
                 </div> 
                 </>
--- src/result.jsx
+++ src/result.jsx
@@ -1,11 +1,13 @@
 import'./result.css'
 function Second (props){
     return(
-        <>
+        <div className='scoreBoard'>
             <div className="ScoreTitle">Score Board</div>
+            <div className='score'>
             <div className="text">Your Score :{props.score}</div>
             <div className="text">Total Score:{props.totalScore}</div>
-        </>
+            </div>
+        </div>
     )
 } 
 export default Second
