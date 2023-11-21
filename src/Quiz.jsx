a6fb8acc098ed04f4097c23fbebc8328f1cb3b09
shikshita23 <shikshitasubedi1@gmail.com>
2023-11-21 13:25:46 +0545

    updated

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -2,53 +2,72 @@ import { useState } from 'react'
 import './Quiz.css'
 import questions from './questions'
 import Second from './result';
-function Quiz(){
-    const[question,setQuestion]=useState(0)
-    const [showResult,setShowResult]=useState(false);
-    const [clickedOption,setClickedOption]=useState(0);
-    const[score,setScore]=useState(0);
-    const nextQuestion=() =>{
+function Quiz() {
+    const [question, setQuestion] = useState(0)
+    const [showResult, setShowResult] = useState(false);
+    const [clickedOption, setClickedOption] = useState(0);
+    const [score, setScore] = useState(0);
+
+    const nextQuestion = () => {
         checkScore();
-        if(question<questions.length-1){
-            setQuestion(question+1)
+        if (question < questions.length - 1) {
+            setQuestion(question + 1)
             setClickedOption(null);
         }
-        else{
+        else {
             setShowResult(true)
         }
-    }    
-    const checkScore=()=>{
-        if(clickedOption===questions[question].answer&&clickedOption !== null){
-            setScore(score+1);
+    }
+    const checkScore = () => {
+        if (clickedOption === questions[question].answer && clickedOption !== null) {
+            setScore(score + 1);
         }
     };
-    const func=(i) =>{
-        setClickedOption(i+1)
+    const questionValues = (i) => {
+        setClickedOption(i + 1) 
     }
-    return(
+    const isOptionChecked = (index) => {
+        const clickedOptionValue = clickedOption === index + 1;  
+        console.log(clickedOptionValue);
+        return clickedOptionValue;
+    }
+    return (
         <div>
             <div className="Heading">Quiz</div>
             <div className="quizBoard">
-                {showResult?(
-                    <Second score={score} totalScore={questions.length}/>
-                    ):(
-                 <> 
-                <div className="questionSection">
-                   <div>{questions[question].qn} </div>      
-                </div>
-                <div className="answerSection">
-                    {questions[question].options.map((option,i )=> {
-                        return(
-                            <>
-                             <label> <input  className="options" type='radio' value={option} name='options' key={i} checked={clickedOption === i + 1} onChange={()=>func(i)}/>{option}</label>  
-                            </>
-                        )
-                    })}
-                </div>
-                <div className='nextSection'>
-                    <button className='nextButton' onClick={nextQuestion}>Next</button>
-                </div> 
-                </>
+                {showResult ? (
+                    <Second score={score} totalScore={questions.length} />
+                ) : (
+                    <>
+                        <div className="questionSection">
+                            <div>{questions[question].qn} </div>
+                        </div>
+                        <div className="answerSection">
+                            {questions[question].options.map((option, index) => {
+                                console.log(index, option)
+                                return (
+                                    <>
+                                        <label> 
+                                            <input 
+                                                className="options" 
+                                                type='radio' 
+                                                value={option} 
+                                                name='options' 
+                                                key={index} 
+                                                checked={isOptionChecked(index)} 
+                                                onChange={() => questionValues(index)} 
+                                            />
+                                            {index}
+                                            {option}
+                                        </label>
+                                    </>
+                                )
+                            })}
+                        </div>
+                        <div className='nextSection'>
+                            <button className='nextButton' onClick={nextQuestion}>Next</button>
+                        </div>
+                    </>
                 )}
             </div>
         </div>
